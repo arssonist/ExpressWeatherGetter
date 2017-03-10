4e1271b5038c15405ee31946cb22364391cfb8b1
Your Name <arssonist@yahoo.com>
2017-03-09 19:07:15 -0500

    add js file; add button style;add view to make alternate home page- need to create post route

--- app.js
+++ app.js
@@ -36,68 +36,68 @@ var app = express();
 
 
 // make call to page get ip from  user
-// app.use(function(req,res,next){
-//   console.log(`${req.method} request for '${req.url}'`);
-//
-// // have to hard code IP, when not using localhost, this would be uncommented
-//   var localhosts = ["localhost", "::1"];
-//   if (localhosts.indexOf(req.ip) >= 0) {
-// // if index is 0 or above then the value is in array
-//     console.log(req.ip)
-//
-//     // hardcode ip
-//     var ip = "104.251.99.162"
-//   } else {
-//     var ip = req.ip;
-//
-//   }
-//   // var ip = req.ip
-//   // console.log(ip);
-//
-// // call ip-api
-//   request(`http://ip-api.com/json/${ip}`,function (error, response, body){
-//
-// //put the entire body back into the req object, then can be used later.
-//     req.ipinfo = JSON.parse(body); //if req is string, it needs to be parsed
-//     next();
-//   })
-// });
-//
-// app.use(function(req,res,next){
-//   // req.ipinfo
-//     console.log('IPINFO:', typeof req.ipinfo, req.ipinfo);
-//
-//
-//     var cityName = req.ipinfo.city;
-//     console.log('CITY:', cityName);
-//
-//     if(!cityName){
-//       next();
-//       return;
-//     }
-// // syntax= url + ?q={location}&APPID={api_key}
-//     var api_key = "28c8970fb188f02790725eac3d906263";
-//     request(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=${api_key}`,function(error, response,body){
-//     req.weatherInfo = JSON.parse(body)
-//
-//     // res.send(req.weatherInfo.name)
-//     console.log(req.weatherInfo)
-//       next();
-//     })
-//
-// })
-//
-// app.get("/", function(req,res){
-//   res.send({
-//     "City Name": req.weatherInfo.name, "Current Conditions": req.weatherInfo.weather[0].main,
-//     "Description": req.weatherInfo.weather[0].description,
-//     "Temp":req.weatherInfo.main.temp,
-//     "Humidity":req.weatherInfo.main.humidity,
-//
-//     "Min Temp":req.weatherInfo.main.temp_min,
-//     "Max Temp":req.weatherInfo.main.temp_max
-//     })
-// })
+app.use(function(req,res,next){
+  console.log(`${req.method} request for '${req.url}'`);
+
+// have to hard code IP, when not using localhost, this would be uncommented
+  var localhosts = ["localhost", "::1"];
+  if (localhosts.indexOf(req.ip) >= 0) {
+// if index is 0 or above then the value is in array
+    console.log(req.ip)
+
+    // hardcode ip
+    var ip = "104.251.99.162"
+  } else {
+    var ip = req.ip;
+
+  }
+  // var ip = req.ip
+  // console.log(ip);
+
+// call ip-api
+  request(`http://ip-api.com/json/${ip}`,function (error, response, body){
+
+//put the entire body back into the req object, then can be used later.
+    req.ipinfo = JSON.parse(body); //if req is string, it needs to be parsed
+    next();
+  })
+});
+
+app.use(function(req,res,next){
+  // req.ipinfo
+    console.log('IPINFO:', typeof req.ipinfo, req.ipinfo);
+
+
+    var cityName = req.ipinfo.city;
+    console.log('CITY:', cityName);
+
+    if(!cityName){
+      next();
+      return;
+    }
+// syntax= url + ?q={location}&APPID={api_key}
+    var api_key = "28c8970fb188f02790725eac3d906263";
+    request(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=${api_key}`,function(error, response,body){
+    req.weatherInfo = JSON.parse(body)
+
+    // res.send(req.weatherInfo.name)
+    console.log(req.weatherInfo)
+      next();
+    })
+
+})
+
+app.get("/", function(req,res){
+  res.send({
+    "City Name": req.weatherInfo.name, "Current Conditions": req.weatherInfo.weather[0].main,
+    "Description": req.weatherInfo.weather[0].description,
+    "Temp":req.weatherInfo.main.temp,
+    "Humidity":req.weatherInfo.main.humidity,
+
+    "Min Temp":req.weatherInfo.main.temp_min,
+    "Max Temp":req.weatherInfo.main.temp_max
+    })
+})
 
 app.get("/enter", function(req,res){
   res.render('index', {
